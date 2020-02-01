3eba0c54e668d650977e4f6aa282ef5a12143e17
riy0 <chac0ptx@gmail.com>
2020-02-01 22:42:41 +0900

    update/test

--- test/index.js
+++ test/index.js
@@ -1,33 +1,25 @@
-'use strict';
+import chai from 'chia';
+import chaiHttp from 'chai-http';
+import server from '../dist/server';
 
-var _chai = require('chai');
-var _chai2 = _interopRequireDefault(_chai);
-
-var _chaiHttp = require('chai-http');
-var _chaiHttp2 = _interopRequireDefault(_chaiHttp);
-
-var _server = require('../dist/server');
-var _server2 = _interopRequireDefault(_server);
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-//const should = _chai2.default.should();
-var should = _chai2.default.should(); 
 /* eslint-env mocha */
 /* eslint no-unused-vars: 0 */
+const should = chai.should();
 
-chai2.default.use(_chaiHttp2.default);
+chai.use(chaiHttp);
 
-describe('/POST entries', function () {
-  it('should create a new entry', function (done) {
-    var newEntry = {
-      title: 'Setting up testing',
-      content: 'I\'ve heard about testing before but never had a reason to take it so serious\n        until the Andela circle 34 bootcamp challenge came in.'
-    };
+const newEntry = {
+  title: 'Test set up',
+  content: `test test test test test test test
+    hoge hoge hoge hoge`
+};
 
-    _chai2.default.request(_server2.default).post('/api/v1/entries').send(newEntry).end(function (err, res) {
+describe('/POST entries', () => {
+  it('should create a new entry', (done) => {
+    chai.request(server)
+    .post('/api/v1/entries')
+    .send(newEntry)
+    .end((err, res) => {
       res.should.have.status(200);
       res.body.data.should.be.a('object');
       done();
@@ -35,11 +27,11 @@ describe('/POST entries', function () {
   });
 });
 
-describe('/GET entries', function() {
-  it('should get all entries', function(done) {
-    _chai2.default .request(_server2.default)
+describe('/GET entries', () => {
+  it('should get all the entries', (done) => {
+    chai.request(server)
       .get('/api/v1/entries')
-      .end(function(err, res) {
+      .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         res.body.data.should.be.a('array');
@@ -49,21 +41,45 @@ describe('/GET entries', function() {
   });
 });
 
-
-describe('/GET/:id entries', function () {
-  it('should get an entry by a given id', function (done) {
-    var newEntry = {
-      title: 'Setting up testing',
-      content: 'I\'ve heard about testing before but never had a reason to take it so serious\n        until the Andela circle 34 bootcamp challenge came in.'
-    };
-
-    _chai2.default.request(_server2.default).post('/api/v1/entries').send(newEntry).end(function (err, res) {
-      _chai2.default.request(_server2.default).get('/api/v1/entries/' + res.body.data.id).end(function (error, response) {
-        response.should.have.status(200);
-        response.body.should.be.a('object');
-        response.body.data.should.be.a('object');
-        done();
+describe('/GET/:id entries', () => {
+  it('should get an entry by a given id', (done) => {
+    chai.request(server)
+      .post('/api/v1/entries')
+      .send(newEntry)
+      .end((err, res) => {
+        chai.request(server)
+          .get(`/api/v1/entries/${res.body.data.id}`)
+          .end((error, response) => {
+            response.should.have.status(200);
+            response.body.should.be.a('object');
+            response.body.data.should.be.a('object');
+            done();
+          });
       });
+  });
+});
+
+
+describe('/PUT/:id entries', () => {
+  it('should update an entry by a given id', (done) => {
+    const entryUpdate = {
+      title: 'New Setting up testing',
+      content: `test test test test test test test
+        until the Andela circle 34 bootcamp challenge came in.`,
+    }
+    chai.request(server)
+    .post('/api/v1/entries')
+    .send(newEntry)
+    .end((err, res) => {
+      chai.request(server)
+        .put(`/api/v1/entries/${res.body.data.id}`)
+        .send(entryUpdate)
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a('object');
+          response.body.data.should.be.a('object');
+          done();
+        });
     });
   });
 });
