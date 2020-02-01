6f9e36d3d5ee917f74243566b69d68447b29b9cb
riy0 <chac0ptx@gmail.com>
2020-02-01 09:32:45 +0900

    update/test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,12 @@
 'use strict';
 
 var _chai = require('chai');
-
 var _chai2 = _interopRequireDefault(_chai);
 
 var _chaiHttp = require('chai-http');
-
 var _chaiHttp2 = _interopRequireDefault(_chaiHttp);
 
 var _server = require('../dist/server');
-
 var _server2 = _interopRequireDefault(_server);
 
 function _interopRequireDefault(obj) {
@@ -17,16 +14,56 @@ function _interopRequireDefault(obj) {
 }
 
 //const should = _chai2.default.should();
+var should = _chai2.default.should(); 
+/* eslint-env mocha */
+/* eslint no-unused-vars: 0 */
 
-_chai2.default.use(_chaiHttp2.default);
+chai2.default.use(_chaiHttp2.default);
+
+describe('/POST entries', function () {
+  it('should create a new entry', function (done) {
+    var newEntry = {
+      title: 'Setting up testing',
+      content: 'I\'ve heard about testing before but never had a reason to take it so serious\n        until the Andela circle 34 bootcamp challenge came in.'
+    };
+
+    _chai2.default.request(_server2.default).post('/api/v1/entries').send(newEntry).end(function (err, res) {
+      res.should.have.status(200);
+      res.body.data.should.be.a('object');
+      done();
+    });
+  });
+});
 
 describe('/GET entries', function() {
-  it('should Get all entries', function(done) {
-    _chai2.default
-      .request(_server2.default)
+  it('should get all entries', function(done) {
+    _chai2.default .request(_server2.default)
       .get('/api/v1/entries')
       .end(function(err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.data.should.be.a('array');
+        res.body.data[0].should.be.a('object');
         done();
       });
+  });
+});
+
+
+describe('/GET/:id entries', function () {
+  it('should get an entry by a given id', function (done) {
+    var newEntry = {
+      title: 'Setting up testing',
+      content: 'I\'ve heard about testing before but never had a reason to take it so serious\n        until the Andela circle 34 bootcamp challenge came in.'
+    };
+
+    _chai2.default.request(_server2.default).post('/api/v1/entries').send(newEntry).end(function (err, res) {
+      _chai2.default.request(_server2.default).get('/api/v1/entries/' + res.body.data.id).end(function (error, response) {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.data.should.be.a('object');
+        done();
+      });
+    });
   });
 });
