6102fbf012cd6ee8c92c67803ce0b59a947c5f9e
riy0 <chac0ptx@gmail.com>
2020-02-02 09:41:09 +0900

    add/test [todo] fix

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import chai from 'chia';
+import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../dist/server';
 
@@ -11,9 +11,21 @@ chai.use(chaiHttp);
 const newEntry = {
   title: 'Test set up',
   content: `test test test test test test test
-    hoge hoge hoge hoge`
+    hoge hoge hoge hoge`,
 };
 
+const invalidEntries = [
+  {
+    title: 'test, only title'
+  },
+  {
+    content: 'test, only content'
+  }
+];
+
+let id = "";
+
+/* TEST THE POST ENDPOINT */
 describe('/POST entries', () => {
   it('should create a new entry', (done) => {
     chai.request(server)
@@ -31,6 +43,25 @@ describe('/POST entries', () => {
       done();
     });
   });
+
+  it('should return an error when any required property is not passed', (done) => {
+    /** generate random number between 0 and 1, 1 inclusive**/
+    let i = Math.floor(Math.random() * 1);
+    let invalidEntry = invalidEntries[i];
+    chai.request(server)
+      .post('/api/v1/entries')
+      .send(invalidEntry)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('status');
+        res.body.status.should.equal('error');
+        res.body.should.have.property('errors');
+        res.body.errors.should.be.a('array');
+        done();
+      })
+  });
+
+
 });
 
 describe('/GET entries', () => {
@@ -65,7 +96,6 @@ describe('/GET/:id entries', () => {
   });
 });
 
-
 describe('/PUT/:id entries', () => {
   it('should update an entry by a given id', (done) => {
     const entryUpdate = {
@@ -77,6 +107,7 @@ describe('/PUT/:id entries', () => {
     .post('/api/v1/entries')
     .send(newEntry)
     .end((err, res) => {
+      id = res.body.data.id;
       chai.request(server)
         .put(`/api/v1/entries/${res.body.data.id}`)
         .send(entryUpdate)
@@ -87,6 +118,21 @@ describe('/PUT/:id entries', () => {
           done();
         });
     });
+  });
+  it('should return an error when any required property is not passed', (done) => {
+    let i = Math.floor(Math.random() * 1);
+    let invalidEntry = invalidEntries[i];
+    chai.request(server)
+      .put(`/api/v1/entries/${id}`)
+      .send(invalidEntry)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('status');
+        res.body.status.should.equal('error');
+        res.body.should.have.property('errors');
+        res.body.errors.should.be.a('array');
+        done();
+      })
   });
 });
 
