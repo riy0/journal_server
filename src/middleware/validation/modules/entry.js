682f85338d22cab3aebac6a9ce57590eb690392e
riy0 <chac0ptx@gmail.com>
2020-02-04 23:33:27 +0900

    update/validation parameter's type

--- src/middleware/validation/modules/entry.js
+++ src/middleware/validation/modules/entry.js
@@ -13,13 +13,13 @@ Entry.create = {
 
 Entry.getById = {
   params: {
-    id: Joi.string().max(16).required(),
+    id: Joi.number().required(),
   },
 };
 
 Entry.update = {
   params: {
-    id: Joi.string().max(16).required(),
+    id: Joi.number().required(),
   },
   body: {
     title: Joi.string().required(),
@@ -29,7 +29,7 @@ Entry.update = {
 
 Entry.delete = {
   params: {
-    id: Joi.string().max(16).required(),
+    id: Joi.number().required(),
   },
 };
 
