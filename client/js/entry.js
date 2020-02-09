7e807eb8a7e2ca10286882b8dca445b94c3a7fe1
riy0 <chac0ptx@gmail.com>
2020-02-09 10:17:56 +0900

    add/edit entry

--- client/js/entry.js
+++ client/js/entry.js
@@ -3,6 +3,7 @@ const errBoxElement = document.getElementById('error-box');
 const entrySubjectField = document.getElementById('write-subject');
 const entryContentField = document.getElementById('write-content');
 const newEntryButton = document.getElementById('new-entry-btn');
+const editEntryButton = document.getElementById('edit-entry-btn');
 
 let errMsgCode;
 
@@ -70,8 +71,100 @@ const addNewEntry = (e) => {
     });
 };
 
+const saveEditEntry = (e, id) => {
+  e.preventDefault();
+
+  const url = `${baseUrl}/entries/${id}`;
+  const title = entrySubjectField.value;
+  const content = entryContentField.value;
+  const validationMsg = validateEntryField(title, content);
+
+  if (validationMsg.title.length !== 0 || validationMsg.content.length !== 0) {
+    let errorMsgs = (validationMsg.title[0]) ? `<li>${(validationMsg.title[0])}</li>` : '';
+    errorMsgs += (validationMsg.content[0]) ? `<li>${(validationMsg.content[0])}</li>` : '';
+    errMsgCode = `<ul id="error-msg">${errorMsgs}</ul>`;
+    errBoxElement.innerHTML = errMsgCode;
+    return;
+  }
+  const editEntryData = {
+    title,
+    content,
+  };
+  const options = {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json, text/plain,  */*',
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(editEntryData),
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((result) => {
+      const { status, message, errors, } = result;
+      let errorMsgs = '';
+      if (status === 'success') {
+        window.location = `${window.location.protocol}//${window.location.host}/client/view-entry.html?id=${id}`;
+      } else if (status === 'error') {
+        if (Object.prototype.hasOwnProperty.call(result, 'errors')) {
+          errors.forEach((error) => {
+            errorMsgs += `<li>${error}</li>`;
+          });
+          errMsgCode = `<ul id="error-msg">${errorMsgs}</ul>`;
+        } else if (Object.prototype.hasOwnProperty.call(result, 'message')) {
+          errorMsgs += `<li>${message}</li>`;
+          errMsgCode = `<ul id="error-msg">${errorMsgs}</ul>`;
+        }
+        errBoxElement.innerHTML = errMsgCode;
+      }
+    })
+    .catch((err) => {
+      const message = `<li>${err}</li>`;
+      errMsgCode = `<ul id="error-msg">${message}</ul>`;
+      errBoxElement.innerHTML = errMsgCode;
+    });
+};
+
+const populateEntryToEdit = (id) => {
+  const url = `${baseUrl}/entries/${id}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain,  */*',
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((result) => {
+      if (result.status === 'success') {
+        entrySubjectField.value = result.data.title;
+        entryContentField.value = result.data.content;
+      }
+    })
+    .catch((err) => {
+      const message = `<li>${err}</li>`;
+      errMsgCode = `<ul id="error-msg">${message}</ul>`;
+      errBoxElement.innerHTML = errMsgCode;
+    });
+};
+
+
 window.onload = () => {
   if (newEntryButton !== null) {
     newEntryButton.addEventListener('click', addNewEntry);
+  }
+  if (editEntryButton !== null) {
+    const currentLocation = new URL(document.location);
+    const params = (currentLocation).searchParams;
+    const entryId = parseInt('10', params.get('id'));
+    populateEntryToEdit(entryId);
+    editEntryButton.addEventListener('click', (e) => {
+      saveEditEntry(e, entryId);
+    });
   }
 };
